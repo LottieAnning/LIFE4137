aba90f800be7c6b4f26572156e44d2170cdb4696
LottieAnning <151543431+LottieAnning@users.noreply.github.com>
2024-07-28 09:02:54 +0100

    Update busco_4_add_headers.sh

--- scripts/busco_4_add_headers.sh
+++ scripts/busco_4_add_headers.sh
@@ -1,4 +1,4 @@
 # Add headers to file
 headers="Species\tComplete BUSCOs\tComplete and single-copy BUSCOs\tComplete and duplicated BUSCOs\tFragmented BUSCOs\tMissing BUSCOs\tTotal BUSCO groups searched\tNumber of scaffolds\tNumber of contigs\tTotal length\tPercent gaps\tScaffold N50\tContigs N50"
-file="all_busco.tsv"
+file="supplementary_file_2.tsv"
 echo -e "$headers\n$(cat "$file")" > temp_file && mv temp_file "$file"
