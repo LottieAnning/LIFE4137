9337832facee986fbf5cf37314e35c268567eba4
LottieAnning <151543431+LottieAnning@users.noreply.github.com>
2024-07-29 16:00:20 +0100

    Update submit.sh

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Source: myself (Charlotte Anning, 20150512)
+# This SLURM job script is used to submit all PAML scripts to the cluster.
+
 # Loop through each script file matching the pattern '*.sh'
 for script in *.sh; do
   echo "Submitting $script"
