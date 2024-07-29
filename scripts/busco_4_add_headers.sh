5be2e5bb4fb1a6fa70507563c64e5a9c2720faee
LottieAnning <151543431+LottieAnning@users.noreply.github.com>
2024-07-29 15:26:56 +0100

    Update busco_4_add_headers.sh

--- scripts/busco_4_add_headers.sh
+++ scripts/busco_4_add_headers.sh
@@ -1,3 +1,6 @@
+# Source: myself (Charlotte Anning, 20150512)
+# This SHELL job script is used to add headers to the BUSCO output.
+
 # Add headers to file
 headers="Species\tComplete BUSCOs\tComplete and single-copy BUSCOs\tComplete and duplicated BUSCOs\tFragmented BUSCOs\tMissing BUSCOs\tTotal BUSCO groups searched\tNumber of scaffolds\tNumber of contigs\tTotal length\tPercent gaps\tScaffold N50\tContigs N50"
 file="supplementary_file_2.tsv"
