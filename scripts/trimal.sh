060faa0c00c16ca362fc69736d1f2754fdbb5094
LottieAnning <151543431+LottieAnning@users.noreply.github.com>
2024-07-29 16:01:02 +0100

    Update trimal.sh

--- scripts/trimal.sh
+++ scripts/trimal.sh
@@ -1,4 +1,5 @@
-# This script prepares data for iqtree and GeneRax and produces the alignment files aa_alignment.aln and nt_alignment.msa.
+# Source: myself (Charlotte Anning, 20150512)
+# This SHELL job script is used to prepare data for IQtree and GeneRax and produces the alignment files aa_alignment.aln and nt_alignment.msa.
 
 # On a macOS or Linux system, install trimal with the following command:
 brew install trimal
