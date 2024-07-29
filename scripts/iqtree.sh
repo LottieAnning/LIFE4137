762a50f85021d143b1aa28950c0adb71aeb1e044
LottieAnning <151543431+LottieAnning@users.noreply.github.com>
2024-07-29 15:53:25 +0100

    Update iqtree.sh

--- scripts/iqtree.sh
+++ scripts/iqtree.sh
@@ -1,4 +1,19 @@
-# This script downloads and runs IQtree
+#!/bin/bash
+
+# Source: myself (Charlotte Anning, 20150512)
+# This SLURM job script is used to submit an IQTREE job to the cluster.
+# Replace [username] with your actual username.
+
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=100g
+#SBATCH --time=2:00:00
+#SBATCH --job-name=iqtree
+#SBATCH --output=/gpfs01/home/[username]/slurm-%x-%j.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=[username]@exmail.nottingham.ac.uk
 
 # First create the files to run iqtree on:
 # With the file all_aa.fasta (created by compiling all the amino acid sequences from the faa files), upload to MAFFT and trim with TRIMAL
